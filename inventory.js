8d1c3b3ff0b3c050b696e1baf7b011c895e8680e
aggarwalsshreya <aggarwalsshreya@gmail.com>
2023-08-02 19:28:20 -0700

    added js file for inventory

---
+++ inventory.js
@@ -0,0 +1,8 @@
+let inventoryForm = document.getElementById("inventoryForm");
+inventoryForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    var apples = document.getElementById("apples").value;
+    var bananas = document.getElementById("bananas").value;
+    var carrots = document.getElementById("carrots").value;
+    var milk = document.getElementById("milk").value;
+  });
