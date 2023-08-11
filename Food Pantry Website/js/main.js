81468ba794599829bba0384fd5b35b0ecaef5151
aggarwalsshreya <40808805+aggarwalsshreya@users.noreply.github.com>
2023-08-11 01:18:29 -0700

    Final

--- Food Pantry Website/js/main.js
+++
@@ -1,6 +0,0 @@
-const spans = document.querySelectorAll('.progress-bar span');
-
-spans.forEach((span) => {
-    span.style.width = span.dataset.width;
-    span.innerHTML = span.dataset.width;
-});
