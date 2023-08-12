4a253321d231b492af4e093c0d142924e7902704
aggarwalsshreya <aggarwalsshreya@gmail.com>
2023-08-12 14:41:17 -0700

    database added

--- spartan.sql
+++ spartan.sql
@@ -2,8 +2,8 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Aug 10, 2023 at 11:47 PM
+-- Host: 127.0.0.1
+-- Generation Time: Aug 12, 2023 at 11:37 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -20,6 +20,29 @@ SET time_zone = "+00:00";
 --
 -- Database: `spartan`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inventory`
+--
+
+CREATE TABLE `inventory` (
+  `id` int(100) NOT NULL,
+  `product` varchar(100) NOT NULL,
+  `category` varchar(100) NOT NULL,
+  `quantity` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `inventory`
+--
+
+INSERT INTO `inventory` (`id`, `product`, `category`, `quantity`) VALUES
+(22, 'Apple', 'Produce', 100),
+(28, 'Toothbrush', 'Toiletries', 200),
+(30, 'Cereal', 'Dry Goods', 99),
+(35, 'Peaches', 'Produce', 85);
 
 -- --------------------------------------------------------
 
@@ -44,6 +67,26 @@ INSERT INTO `login` (`job`, `username`, `password`) VALUES
 ('staff', 'armica', 'abcd'),
 ('shopper', 'timmy', '1234'),
 ('shopper', 'carly', '1234');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `inventory`
+--
+ALTER TABLE `inventory`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `inventory`
+--
+ALTER TABLE `inventory`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
